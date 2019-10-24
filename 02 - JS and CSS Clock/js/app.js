a52dd4044ffcc073822c293efd0a96652bbab808
Kaniah Dunn <kaniahdunn@gmail.com>
2019-10-24 15:53:38 -0400

    day 2 js project challenge

---
+++ 02 - JS and CSS Clock/js/app.js
@@ -0,0 +1,29 @@
+
+
+// get the seconds, min and hour hands
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+//set time function
+function setDate() {
+  //create obejct for new date
+  const now = new Date();
+
+  //create con
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
